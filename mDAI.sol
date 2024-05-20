33aa99cb47def37a92e8cf697acf15f5f599fce1
murioil <>
2024-05-20 02:06:29 -0700

    update

--- mDAI.sol
+++ mDAI.sol
@@ -15,18 +15,30 @@ interface IERC20 {
 contract ERC20 {
     string public name = 'Murioil-DAI';
     string public symbol = 'mDAI';
+    string  public version = '1';
     uint public decimals = 18;
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
     mapping(address => bool) public isUser;
+    mapping (address => uint) public nonces;
+    bytes32 public DOMAIN_SEPARATOR;
+    // bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)");
+    bytes32 public constant PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
     address[] public users;
     address public minter;
     event Approval(address indexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);    
 
-    constructor() {
+    constructor(uint256 chainId_) {
         minter = msg.sender;
+        DOMAIN_SEPARATOR = keccak256(abi.encode(
+            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+            keccak256(bytes(name)),
+            keccak256(bytes(version)),
+            chainId_,
+            address(this)
+        ));
     }
 
     function changeMinter(address newminter) external returns (bool) {
@@ -96,5 +108,17 @@ contract ERC20 {
         allowance[from][msg.sender] -= value;
         _transfer(from, to, value);
         return true;
+    }
+    // --- Approve by signature ---
+    function permit(address holder, address spender, uint256 nonce, uint256 expiry, bool allowed, uint8 v, bytes32 r, bytes32 s) external {
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01",DOMAIN_SEPARATOR,keccak256(abi.encode(PERMIT_TYPEHASH,holder,spender,nonce,expiry,allowed))));
+        require(holder != address(0), "Dai/invalid-address-0");
+        require(holder == ecrecover(digest, v, r, s), "Dai/invalid-permit");
+        require(expiry == 0 || block.timestamp <= expiry, "Dai/permit-expired");
+        require(nonce == nonces[holder], "Dai/invalid-nonce");
+        nonces[holder]+=1;
+        uint wad = allowed ? type(uint256).max : 0;
+        allowance[holder][spender] = wad;
+        emit Approval(holder, spender, wad);
     }
 }
